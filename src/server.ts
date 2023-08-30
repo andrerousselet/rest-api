d36e97506eb0befe5e153073cc8ed3135f93a526
Andre Rousselet <andre098@hotmail.com>
2023-08-30 18:37:48 -0300

    feat(server/transactions): register transactions plugin and add POST /transactions with body validation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/hello", async (req, res) => {
-  const transaction = await knex("transactions").select("*");
-  return transaction;
+app.register(transactionsRoutes, {
+  prefix: "transactions",
 });
 
 app.listen({ port: env.PORT }).then(() => {
