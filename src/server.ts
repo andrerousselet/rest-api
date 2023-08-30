b95f0881709809ccac615fbd75304ae137b1478b
Andre Rousselet <andre098@hotmail.com>
2023-08-30 18:00:54 -0300

    feat(server.ts/database.ts): use env variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import crypto from "node:crypto";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
@@ -9,6 +10,6 @@ app.get("/hello", async (req, res) => {
   return transaction;
 });
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server running on port 3333");
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`HTTP server running on port ${env.PORT}`);
 });
