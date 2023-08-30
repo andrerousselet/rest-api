5d292b80cdcac917774517b657d712f4863ba88b
Andre Rousselet <andre098@hotmail.com>
2023-08-30 17:10:25 -0300

    feat(knex-config): adjust knex config (knexfile.ts) and add migrations folder inside db folder

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,15 @@
 import { knex as knexSetup, Knex } from "knex";
 
-export const knex: Knex = knexSetup({
+export const config: Knex.Config = {
   client: "sqlite",
   connection: {
     filename: "./db/app.db",
   },
   useNullAsDefault: true,
-});
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+};
+
+export const knex = knexSetup(config);
